4f96737ff089e4f77d022d1058fcf50e620b723a
rahulsamant37 <rahulsamantcoc2@gmail.com>
2025-03-04 20:32:14 +0530

    correcting output path

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,6 @@
-OUTPUT_PATH = "output/"
+import os
+
+# Use absolute path for output
+OUTPUT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'output/'))
 
 DRIVER_EXECUTABLE_PATH = None
